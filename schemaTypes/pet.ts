d49bac3482698e02a0e8b8ba1d336ebfb5749245
Josiah-10year <84046189+Josiah-10year@users.noreply.github.com>
2024-02-24 15:58:21 -0400

    cratio of schema

---
+++ schemaTypes/pet.ts
@@ -0,0 +1,14 @@
+import {defineField, defineType} from 'sanity'
+
+export default {
+    name: 'pet',
+    type: 'document',
+    title: 'Pet',
+    fields: [
+        {
+            name: 'name',
+            type: 'string',
+            title: 'Name'
+        }
+    ]
+}
